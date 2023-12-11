dd5ffb421acb2e372c99e74348d05f795cf24d3f
Neil Xu <neilxu@Neils-MacBook-Pro.local>
2023-12-10 20:35:19 -0500

    initial frontend work

--- src/app.py
+++ src/app.py
@@ -1,10 +1,53 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify, send_file
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
+# Set the upload folder
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Ensure the upload folder exists
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+# Set allowed file extensions
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'photo' not in request.files:
+        return jsonify({'message': 'No file part'}), 400
+
+    file = request.files['photo']
+
+    if file.filename == '':
+        return jsonify({'message': 'No selected file'}), 400
+
+    if file and allowed_file(file.filename):
+        # Save the uploaded file
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
+
+        # Create a temporary link to the uploaded file
+        image_link = '/uploads/' + filename
+
+        return jsonify({'message': 'Upload successful!', 'image_link': image_link})
+
+    return jsonify({'message': 'Invalid file format. Allowed formats: png, jpg, jpeg, gif'}), 400
+
+@app.route('/uploads/<filename>')
+def serve_image(filename):
+    # print("getting image", os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return send_file(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ src/static/index.js
@@ -0,0 +1,55 @@
+function uploadImage() {
+    const input = document.getElementById('file-input');
+    const file = input.files[0];
+
+    if (file) {
+        const formData = new FormData();
+        formData.append('photo', file);
+
+        fetch('/upload', {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+            const responseMessage = document.getElementById('response-message');
+            
+            if (data.message === 'Upload successful!') {
+                responseMessage.innerHTML = `<div class="alert alert-success" role="alert">${data.message}</div>`;
+            } else {
+                // Display error message
+                responseMessage.innerHTML = `<div class="alert alert-danger" role="alert">${data.message}</div>`;
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            const responseMessage = document.getElementById('response-message');
+            responseMessage.innerHTML = '<div class="alert alert-danger" role="alert">Upload failed. Please try again.</div>';
+        });
+    } else {
+        const responseMessage = document.getElementById('response-message');
+        responseMessage.innerHTML = '<div class="alert alert-warning" role="alert">No file selected. Please choose a file to upload.</div>';
+    }
+}
+
+
+
+function chooseFile() {
+    document.getElementById('file-input').click();
+}
+
+function displayImage() {
+    const input = document.getElementById('file-input');
+    const img = document.getElementById('preview-image');
+
+    const file = input.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            img.src = e.target.result;
+            img.style.display = 'block';
+        };
+        reader.readAsDataURL(file);
+    }
+}
